11632970a3a6131fb4e5b06d34bee8d8afd3c19e
gotzathe <gotzathe@gmail.com>
2017-03-26 23:40:18 -0500

    Add get_pid

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Kahoot PID bruteforcer
 The Kahoot PID bruteforcer uses the Kahoot token request API implemented in the ukahoot token server to discover Kahoot PIDs via bruteforcing
+
 ## Building
+Make sure you have the OpenSSl developer libraries installed and run `make` in a terminal window
--- src/main.c
+++ src/main.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 
 #include "request.h"
 
 char const digits[] = "0123456789";
-char* itoa(int i, char b[]){
-    char* p = b;
-    if(i<0) {
-        *p++ = '-';
-        i *= -1;
+void prepend(char* s, const char* t) {
+    size_t len = strlen(t);
+    size_t i;
+    memmove(s + len, s, strlen(s) + 1);
+    for (i = 0; i < len; ++i) {
+        s[i] = t[i];
     }
-    int shifter = i;
-    do {
-        ++p;
-        shifter = shifter/10;
-    } while(shifter);
-    *p = '\0';
-    do {
-        *--p = digits[i%10];
-        i = i/10;
-    } while(i);
-    return b;
 };
-char* pid = "0000000";
+char* get_pid(char* pid) {
+	int ipid = atoi(pid);
+	++ipid;
+	sprintf(pid, "%d", ipid);
+	while (strlen(pid) < 4) {
+		prepend(pid, "0");
+	}
+	return pid;
+};
+char* pid;
 
 int main(int argc, char* argv[]) {
 	//
