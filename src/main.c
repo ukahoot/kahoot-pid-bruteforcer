7c9cecd85f2bbbc76d2f8bafcc0771f085695f05
gotzathe <gotzathe@gmail.com>
2017-03-26 23:13:08 -0500

    Add itoa and pid

--- src/main.c
+++ src/main.c
@@ -4,6 +4,27 @@
 
 #include "request.h"
 
+char const digits[] = "0123456789";
+char* itoa(int i, char b[]){
+    char* p = b;
+    if(i<0) {
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do {
+        ++p;
+        shifter = shifter/10;
+    } while(shifter);
+    *p = '\0';
+    do {
+        *--p = digits[i%10];
+        i = i/10;
+    } while(i);
+    return b;
+};
+char* pid = "0000000";
+
 int main(int argc, char* argv[]) {
 	//
 };
